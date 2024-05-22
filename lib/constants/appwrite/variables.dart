7eea403a5e85f276d49856656bd5346759ec9f3f
Tilak Raj <34453654+Tilak1778@users.noreply.github.com>
2024-05-22 10:04:30 +0530

    Update variables.dart

--- lib/constants/appwrite/variables.dart
+++ lib/constants/appwrite/variables.dart
@@ -1,7 +1,7 @@
 /// User ID for AppWrite project
-String appWriteUserID = '6649a93d001fdcb1b64f';
+String appWriteUserID = '';
 
 /// Storage bucket IDs of AppWrite project
 Map storageBucketIDs = {
-  'image': '6649a972000adf8781ef',
+  'image': '',
 };
